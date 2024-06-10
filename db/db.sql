2c2239e14c4e757157b06b48e7a9e3ae9e134464
Md. Faisal Alam <md.alamfaisal@gmail.com>
2024-06-10 14:16:28 +0600

    database dumping

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,5 @@
+USE ums;
+
 CREATE TABLE users (
 	id INT auto_increment NOT NULL,
 	username varchar(100) NOT NULL,
@@ -19,3 +21,11 @@ DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
 
 ALTER TABLE users ADD role_id INTEGER NOT NULL;
+
+INSERT INTO ums.users (username,email,password,role_id) VALUES
+	 ('admin','admin@admin.com','7b902e6ff1db9f560443f2048974fd7d386975b0',1),
+	 ('Faisal','faisal@user.com','7b902e6ff1db9f560443f2048974fd7d386975b0',2);
+
+INSERT INTO ums.roles (name) VALUES
+	 ('admin'),
+	 ('user');
